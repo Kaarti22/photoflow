f8fa1b50d868acd99f9404dc12d3f773d3c085e4
GarimaUttam <garimauttam191203@gmail.com>
2025-03-18 20:13:49 +0530

    basic configuration of app.js and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: __dirname + "/config.env" });
+
+
 const app = require("./app");
 
 
-const port = 3000;
+const port = process.env.PORT || 3000 ;
 
 const server = app.listen(port, () => {
     console.log(`App running on port ${port}`);
