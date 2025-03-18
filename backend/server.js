427421eb808a263cbc2a1705cc1f48bfc590002d
GarimaUttam <garimauttam191203@gmail.com>
2025-03-18 20:41:51 +0530

    MVC (bcnd - M, C)& connecting to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const manogoose = require("mongoose");
 dotenv.config({ path: __dirname + "/config.env" });
 
-
 const app = require("./app");
+const { default: mongoose } = require("mongoose");
 
+mongoose
+  .connect(process.env.DB)
+  .then(() => {
+    console.log("DB connection successfull");
+  })
+  .catch((err) => console.log(err));
 
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
