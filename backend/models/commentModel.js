754fd9cb5a279f1a1d4e19d75e24a4829df253d5
GarimaUttam <garimauttam191203@gmail.com>
2025-03-20 18:55:07 +0530

    create-post controller, post- model, comment-model

---
+++ backend/models/commentModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: [true, "Comment text is required."],
+        trim: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    createdAt: {
+        type: Data,
+        default: Data.now,
+    },
+},
+    { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
