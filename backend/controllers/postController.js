67ba3825d094ed66c54120c58741e6ccee437c48
GarimaUttam <garimauttam191203@gmail.com>
2025-03-21 09:33:48 +0530

    likeOrdislike port , add comment , delete post done

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,7 +1,7 @@
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const sharp = require("sharp");
-const { uploadToCloudinary } = require("../utils/cloudinary");
+const { uploadToCloudinary, cloudinary } = require("../utils/cloudinary");
 const Post = require("../models/postModel");
 const User = require("../models/userModel");
 const { post } = require("../routes/userRoutes");
@@ -25,84 +25,215 @@ exports.createPost = catchAsync(async (req, res, next) => {
 
   const fileUri = `data:image/jpeg;base64,${optimizedImageBuffer.toString(
     "base64"
-    )}`;
-    
-    const cloudResponse = await uploadToCloudinary(fileUri);
+  )}`;
 
-    let post = await Post.create({
-        caption,
-        image: {
-            url: cloudResponse.secure_url,
-            publicId: cloudResponse.public_id,
-        },
-        user: userId,
-    });
+  const cloudResponse = await uploadToCloudinary(fileUri);
 
+  let post = await Post.create({
+    caption,
+    image: {
+      url: cloudResponse.secure_url,
+      publicId: cloudResponse.public_id,
+    },
+    user: userId,
+  });
 
-    // add post to users posts
-    const user = await User.findById(userId);
+  // add post to users posts
+  const user = await User.findById(userId);
 
-    if (user) {
-        user.posts.push(post.id);
-        await user.save({ validataBeforeSave: false });
-    }
+  if (user) {
+    user.posts.push(post.id);
+    await user.save({ validataBeforeSave: false });
+  }
 
-    post = await post.populate({
-        path: 'user',
-        select: "username email bio profilePicture",
-    });
+  post = await post.populate({
+    path: "user",
+    select: "username email bio profilePicture",
+  });
 
-    return res.status(200).json({
-        status: "Success",
-        message: "Post Created",
-        data: {
-            post,
-        },
-    });
-
+  return res.status(200).json({
+    status: "Success",
+    message: "Post Created",
+    data: {
+      post,
+    },
+  });
 });
 
 exports.getAllPost = catchAsync(async (req, res, next) => {
-    const posts = await Post.find()
-        .populate({
-            path: "user",
-            select: "username profilePicture bio",
-        })
-        .populate({
-            path: "comments",
-            select: "text user",
-            populate: {
-                path: "user",
-                select: "username profilePicture",
-            },
-        }).sort({ createdAt: -1 });
-    
-    return res.status(200).json({
-        status: "success",
-        results: posts.length,
-        data: {
-            posts,
-        },
-    });
+  const posts = await Post.find()
+    .populate({
+      path: "user",
+      select: "username profilePicture bio",
+    })
+    .populate({
+      path: "comments",
+      select: "text user",
+      populate: {
+        path: "user",
+        select: "username profilePicture",
+      },
+    })
+    .sort({ createdAt: -1 });
+
+  return res.status(200).json({
+    status: "success",
+    results: posts.length,
+    data: {
+      posts,
+    },
+  });
 });
 
 exports.getUserPosts = catchAsync(async (req, res, next) => {
-    const userId = req.params.id;
+  const userId = req.params.id;
 
-    const posts = await Post.find({ user: userId }).populate({
-        path: "comments",
-        select: "text user",
-        populate: {
-            path: "user",
-            select: "username profilePicture",
-        },
-    }).sort({ createdAt: -1 });
+  const posts = await Post.find({ user: userId })
+    .populate({
+      path: "comments",
+      select: "text user",
+      populate: {
+        path: "user",
+        select: "username profilePicture",
+      },
+    })
+    .sort({ createdAt: -1 });
 
+  return res.status(200).json({
+    status: "success",
+    results: posts.length,
+    data: {
+      posts,
+    },
+  });
+});
+
+exports.saveOrUnsavePost = catchAsync(async (req, res, next) => {
+  const userId = req.user._id;
+  const postId = req.params.postId;
+
+  const user = await User.findById(userId);
+
+  if (!user) return next(new AppError("User not found", 404));
+
+  const isPostSave = user.savedPosts.includes(postId);
+  if (isPostSave) {
+    user.savedPosts.pull(postId);
+    await user.save({ validataBeforeSave: false });
     return res.status(200).json({
-        status: "success",
-        results: posts.length,
-        data: {
-            posts,
-        },
+      status: "success",
+      message: "post unsaved successfully.",
+      data: {
+        user,
+      },
     });
+  } else {
+    user.savedPosts.push(postId);
+    await user.save({ validateBeforeSave: false });
+    return res.status(200).json({
+      status: "success",
+      message: "Post saved successfully.",
+      data: {
+        user,
+      },
+    });
+  }
+});
+
+exports.deletePost = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const usrId = req.user._id;
+
+  const post = await Post.findById(id).populate("user");
+
+  if (!post) {
+    return next(new AppError("Post not found", 404));
+  }
+
+  if (post.user._id.toString() !== userId.toString()) {
+    return next(
+      new AppError("You are not authorized to delete this post", 403)
+    );
+  }
+  // remove the post from the user posts
+  await User.updateOne({ _id: userId }, { $pull: { posts: is } });
+
+  // remove this post posts from the users save list
+  await User.updateMany({ savedPosts: id }, { $pull: { savedPost: id } });
+
+  // remove the comment of this post
+  await Comment.deleteMany({ post: id });
+
+  // remove immage from the cloudinnary
+  if (post.image.publicId) {
+    await cloudinary.uploader.destroy(post.image.publicId);
+  }
+
+  // remove the posts
+  await Post.findByIdAndDelete(id);
+
+  res.status(200).json({
+    status: "success",
+    message: "Post deleted successfully",
+  });
+});
+
+exports.likeOrDislikePost = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user._id;
+
+  const post = await Post.findById(id);
+  if (!post) return next(new AppError("Post not found", 404));
+
+  const isLiked = post.likes.includes(userId);
+
+  if (isLiked) {
+    await Post.findByIdAndUpdate(
+      id,
+      { $pull: { likes: userId } },
+      { new: true }
+      );
+      
+      return res.status(200).json({
+          status: "success",
+          message: "Post disliked successfully"
+      });
+  } else {
+      await Post.findByIdAndUpdate(id, { $addToSet: { likes: userId } }, { new: true });
+      return res.status(200).json({
+          status: "success",
+          message: "Post liked successfully",
+      });
+  }
+});
+
+exports.addComment = catchAsync(async (res, res, next) => {
+    const { id: postId } = req.params;
+    const userId = req.user._id;
+    
+    const { text } = req.body;
+
+    const post = await Post.findById(postId);
+
+    if (!post) return next(new AppError("Post not found", 404));
+
+    if (!text) return next(new AppError("Comment text is required", 400));
+
+    const comment = await Comment.create({
+        text, user: userId, createdAt: Date.now(),
+    });
+
+    post.comments.push(comment);
+    await post.save({ validateBeforeSave: false });
+
+    await comment.populate({ path: "user", select: "username profilePicture bio", });
+
+    res.status(201).json({
+        status: "success",
+        message: "Comment added successfully",
+        data: {
+            comment,
+        },
+    })
+
 });
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const isAuthenticated = require("../middleware/isAuthenticated");
 const upload = require("../middleware/multer");
-const { createPost, getAllPost, getUserPosts } = require("../controllers/postController");
+const { createPost, getAllPost, getUserPosts, saveOrUnsavePost, deletePost, likeOrDislikePost, addComment } = require("../controllers/postController");
 
 const router = express.Router();
 
@@ -15,5 +15,9 @@ router.post(
 
 router.get("/all", getAllPost);
 router.get("/user-post/:id", getUserPosts)
+router.post("/save-unsave-post/:postId", isAuthenticated, saveOrUnsavePost);
+router.delete("/delete-post/:id", isAuthenticated, deletePost);
+router.post("/like-dislike/:id", isAuthenticated, likeOrDislikePost)
+router.post("/comment/:id", isAuthenticated, addComment);
 
 module.exports = router;
