11292b6d3b4a7bb379065faafc7c55f2c7ce74da
GarimaUttam <garimauttam191203@gmail.com>
2025-03-22 08:54:34 +0530

    frontend set up

---
+++ frontend/src/app/auth/signup/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Signup from '../../../components/Auth/Signup'
+
+const SignUpPage = () => {
+  return (
+    <><Signup/></>
+  )
+}
+
+export default SignUpPage
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,9 +4,9 @@ import Home from '../components/Home/Home';
 const HomePage = () => {
   return (
     <div>
-      <Home/>
+      <Home />
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
