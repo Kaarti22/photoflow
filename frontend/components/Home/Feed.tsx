aa59167f600f2d694773f66f98256bb86921ffe8
GarimaUttam <garimauttam191203@gmail.com>
2025-03-26 10:52:48 +0530

    configuring nextjs for cloudinary for feed page

--- frontend/components/Home/Feed.tsx
+++ frontend/components/Home/Feed.tsx
@@ -1,9 +1,129 @@
-import React from 'react'
+"use client";
+import { BASE_API_URL } from "@/server";
+import { RootState } from "@/store/store";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { handleAuthRequest } from "../utils/apiRequest";
+import axios from "axios";
+import { setPost } from "@/store/postSlice";
+import { Bookmark, HeartIcon, Loader, MessageCircle, Send } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import DotButton from "../Helper/DotButton";
+import Image from "next/image";
+import Comment from "../Helper/Comment";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default Feed
+  const user = useSelector((state: RootState) => state.auth.user);
+  const posts = useSelector((state: RootState) => state.posts.posts);
+
+  const [comment, setComment] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getAllPost = async () => {
+      const getAllPostReq = async () =>
+        await axios.get(`${BASE_API_URL}/posts/all`);
+      const result = await handleAuthRequest(getAllPostReq, setIsLoading);
+
+      if (result) {
+        dispatch(setPost(result.data.data.posts));
+      }
+    };
+    getAllPost();
+  }, [dispatch]);
+
+  const handleLikeDislike = async (is: string) => {};
+
+  const handleSaveUnsave = async (id: string) => {};
+
+  const handleComment = async (id: string) => {};
+
+  // handle loading state
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center flex-col">
+        <Loader className="animate-spin" />
+      </div>
+    );
+  }
+
+  if (posts.length < 1) {
+    return (
+      <div className="text-3xl m-8 text-center capitalize font-bold">
+        No post to show
+      </div>
+    );
+  }
+
+  return (
+    <div className="mt-20 w-[70%] mx-auto">
+      {/* Main posts */}
+      {posts.map((post) => {
+        return (
+          <div key={post._id} className="mt-8">
+            <div className="flex items-center justify-between">
+              {/* UserInfo */}
+              <div className="flex items-center space-x-2 ">
+                <Avatar className="w-9 h-9">
+                  <AvatarImage
+                    src={post.user?.profilePicture}
+                    className="h-full w-full"
+                  />
+                  <AvatarFallback>ME</AvatarFallback>
+                </Avatar>
+                <h1>{post.user?.username}</h1>
+              </div>
+              {/* DotButton */}
+              <DotButton />
+            </div>
+            {/* Image */}
+            <div className="mt-2">
+              <Image
+                src={`${post.image?.url}`}
+                alt="Post"
+                width={400}
+                height={400}
+                className="w-full"
+              />
+            </div>
+            <div className="mt-3 flex items-center justify-between">
+              <div className="flex items-center space-x-4">
+                <HeartIcon className="cursor-pointer" />
+                <MessageCircle className="cursor-pointer" />
+                <Send classNamecursor-pointer />
+              </div>
+              <Bookmark className="cursor-pointer" />
+            </div>
+            <h1 className="mt-2 text-sm font-semibold">
+              {post.likes.length} likes
+            </h1>
+            <p className="mt-2 font-medium">{post.caption}</p>
+            {/* Comments */}
+            <Comment />
+            <div className="mt-2 flex items-center">
+              <input
+                type="text"
+                placeholder="Add a Comment...."
+                className="flex-1 placeholder:text-gray-800 outline-none"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              />
+              <p
+                role="button"
+                className="text-sm font-semibold text-blue-700 cursor-pointer"
+                onClick={() => {
+                  handleComment(post._id);
+                }}
+              ></p>
+            </div>
+            <div className="pb-6 border-b-2"></div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Feed;
