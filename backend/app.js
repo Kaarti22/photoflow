01992963c0d63245288a695673d31c486d721c5e
GarimaUttam <garimauttam191203@gmail.com>
2025-03-27 10:44:16 +0530

    fixed CORS

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://photoflow-ecru.vercel.app"],
     credentials: true,
   })
 );
